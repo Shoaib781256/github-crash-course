41fa0820da0af130a2efeb2b52ef9798a28004ee
Shoaib781256 <mohammedshoaibc2@gmail.com>
2024-01-30 21:09:01 +0530

    Added isAbove 18 fn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const age = 20;
-const name = 'shoaib';
+const name1 = 'shoaib';
 const dob = 21/12/2003;
 
 
 function sayName(){
-    console.log('My Name is '+ name)
+    console.log('My Name is '+ name1)
+}
+
+function isAbove18(){
+    return age >= 18
 }
